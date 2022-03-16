1688cf3f7e0c071cf01e9b5fa5338303e9c94ed9
amongjiwo <dahlanakhmad@gmail.com>
2022-03-16 21:13:26 +0700

    Create Variable Javascript

---
+++ main.js
@@ -0,0 +1,16 @@
+const namaEvent = "MotoGP";
+const lokasi = "Sirkuit Internasional Mandalika";
+const tanggalEvent = "18 - 20 Maret 2022";
+
+console.log(`Jangan lupa saksikan ${namaEvent} yang akan berlangsung pada ${tanggalEvent} di ${lokasi}`);
+// Output: Jangan lupa saksikan MotoGP yang akan berlangsung pada 18 - 20 Maret 2022 di Sirkuit Internasional Mandalika
+
+let hargaMinyakGorengKemasan = 20000;
+hargaMinyakGorengKemasan = 14000;
+console.log(hargaMinyakGorengKemasan);
+
+const apakahKoruptorMerugikanNegara = true;
+const apakahHukumanKoruptorSudahAdil = false;
+
+console.log(apakahKoruptorMerugikanNegara);
+console.log(apakahHukumanKoruptorSudahAdil);
